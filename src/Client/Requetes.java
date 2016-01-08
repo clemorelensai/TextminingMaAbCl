592f968500be76dfa342fc46f124ae59d3e560fa
ManonYG <manon.giraud@ensai.fr>
2016-01-08 15:01:12 +0100

    Modification de l'interface Requetes

--- src/Client/Requetes.java
+++ src/Client/Requetes.java
@@ -7,29 +7,29 @@ import indexation.LectureFichier;
 
 public interface Requetes {
 	
-	public ArrayList<Integer> requeteSimple(String terme, LectureFichier index);
+	public ArrayList<Integer> requeteSimple(String terme);
 	// retourne les documents les plus pertinents dans la recherche de "terme"
 
 	
-	public TreeSet<String> requeteAnd(ArrayList<String> termes, LectureFichier index);
+	public TreeSet<String> requeteAnd(ArrayList<String> termes);
 	// retourne les documents les plus pertinents contenant les termes "termes"
 	
-	public TreeSet<String> requeteOr(ArrayList<String> termes, LectureFichier index);
+	public TreeSet<String> requeteOr(ArrayList<String> termes);
 	// retourne les documents les plus pertinents contenant au moins l'un des termes parmi "termes"
 	
-	public TreeSet<String> requeteXor(ArrayList<String> termes, LectureFichier index);
+	public TreeSet<String> requeteXor(ArrayList<String> termes);
 	// retourne les documents les plus pertinents contenant un seul des termes parmi "termes"
 	
-	public TreeSet<String> requeteNear(ArrayList<String> termes, LectureFichier index);
+	public TreeSet<String> requeteNear(ArrayList<String> termes);
 	// retourne les documents les plus pertinents contenant les termes "termes" collés entre eux
 	
-	public TreeSet<String> requeteNot(ArrayList<String> termes, LectureFichier index);
+	public TreeSet<String> requeteNot(ArrayList<String> termes);
 	// retourne les documents les plus pertinents ne contenant PAS les termes "termes"
 	
-	public TreeSet<String> requeteSentence(ArrayList<String> termes, LectureFichier index);
+	public TreeSet<String> requeteSentence(ArrayList<String> termes);
 	// retourne les documents les plus pertinents contenant les termes "termes" dans une même phrase
 	
-	public TreeSet<String> requeteParagraph(ArrayList<String> termes, LectureFichier index);
+	public TreeSet<String> requeteParagraph(ArrayList<String> termes);
 	// retourne les documents les plus pertinents contenant les termes "termes" dans un même paragraphe
 	
 
--- src/Client/RequetesArbre.java
+++ src/Client/RequetesArbre.java
@@ -6,52 +6,52 @@ import java.util.TreeSet;
 import indexation.LectureFichier;
 
 public class RequetesArbre implements Requetes {
-	
+	LectureFichier index;
 
 	@Override
-	public ArrayList<Integer> requeteSimple(String terme, LectureFichier index) {
+	public ArrayList<Integer> requeteSimple(String terme) {
 
 		return null;
 	}
 
 	@Override
-	public TreeSet<String> requeteAnd(ArrayList<String> termes, LectureFichier index) {
+	public TreeSet<String> requeteAnd(ArrayList<String> termes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public TreeSet<String> requeteOr(ArrayList<String> termes, LectureFichier index) {
+	public TreeSet<String> requeteOr(ArrayList<String> termes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public TreeSet<String> requeteXor(ArrayList<String> termes, LectureFichier index) {
+	public TreeSet<String> requeteXor(ArrayList<String> termes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public TreeSet<String> requeteNear(ArrayList<String> termes, LectureFichier index) {
+	public TreeSet<String> requeteNear(ArrayList<String> termes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public TreeSet<String> requeteNot(ArrayList<String> termes, LectureFichier index) {
+	public TreeSet<String> requeteNot(ArrayList<String> termes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public TreeSet<String> requeteSentence(ArrayList<String> termes, LectureFichier index) {
+	public TreeSet<String> requeteSentence(ArrayList<String> termes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public TreeSet<String> requeteParagraph(ArrayList<String> termes, LectureFichier index) {
+	public TreeSet<String> requeteParagraph(ArrayList<String> termes) {
 		// TODO Auto-generated method stub
 		return null;
 	}
